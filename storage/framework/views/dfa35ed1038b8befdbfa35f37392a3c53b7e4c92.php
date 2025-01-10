486e13520eedbf333e647ac4d4c68f9feec030dc
nell4438 <98459805+nell4438@users.noreply.github.com>
2025-01-10 11:46:02 +0800

    Add files via upload

---
+++ storage/framework/views/dfa35ed1038b8befdbfa35f37392a3c53b7e4c92.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" size="50" href="css/image/logo.jpg" type="image/x-icon">
+    <title>Hazard Patrol Monitoring</title>
+</head>
+<body>
+    <div id="app">
+        <App></App>
+    </div>
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+</html><?php /**PATH C:\Apache24\htdocs\HazardPatrolMonitoring\resources\views/layouts/app.blade.php ENDPATH**/ ?>
